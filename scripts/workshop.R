e16cc0912392e2cd9ae019c02716ea8f7d69a29c
Dr Peter Humburg <peter.humburg@mq.edu.au>
2018-11-22 14:26:18 +1100

    Initial commit

---
+++ scripts/workshop.R
@@ -0,0 +1,4 @@
+## Use this file to follow along with the live coding exercises.
+## The csv file containing the data is in the 'data/' directory.
+## If you want to save plots you created, place them in the 'figures/' directory.
+## You can create additional R files in the 'scripts/' directory.
