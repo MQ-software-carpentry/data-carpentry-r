8f909a2e586ccf41d8ffc5983530fdfb97dca0f4
Dr Peter Humburg <peter.humburg@mq.edu.au>
2018-11-29 13:19:06 +1100

    select

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -1,1 +1,7 @@
 # dplyr and tidyr
+library(tidyverse)
+
+interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
+
+select(interviews, village, no_membrs, years_liv)
+select(interviews, village:rooms)
