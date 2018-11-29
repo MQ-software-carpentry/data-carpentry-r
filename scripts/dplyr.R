fc685a0b78fb44104a541c69da6acf08ae2d7004
Dr Peter Humburg <peter.humburg@mq.edu.au>
2018-11-29 13:29:39 +1100

    filter

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -5,3 +5,7 @@ interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
 
 select(interviews, village, no_membrs, years_liv)
 select(interviews, village:rooms)
+
+# filtering
+filter(interviews, village == "God", rooms > 2)
+filter(interviews, 1:nrow(interviews) %in% 1:10)
