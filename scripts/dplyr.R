7a3ba746062d70d302e99e915316253631b61032
Dr Peter Humburg <peter.humburg@mq.edu.au>
2018-11-29 13:46:37 +1100

    pipes

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -9,3 +9,12 @@ select(interviews, village:rooms)
 # filtering
 filter(interviews, village == "God", rooms > 2)
 filter(interviews, 1:nrow(interviews) %in% 1:10)
+
+interviews2 <- filter(interviews, village == "God")
+interviews_god <- select(interviews2, no_membrs, years_liv)
+
+interviews_god <- select(filter(interviews, village == "God"),
+                         no_membrs, years_liv)
+
+interviews_god <- interviews %>% filter(village=="God")%>%
+  select(no_membrs, years_liv)
